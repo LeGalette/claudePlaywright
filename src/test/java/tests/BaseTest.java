6e42433e9e03603443eb0b7851e38c52c781db88
LeGalette <evilape@web.de>
2025-01-05 16:12:00 +0100

    Add Playwright test automation framework

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected Playwright playwright;
+    protected Browser browser;
+    protected Page page;
+
+    @BeforeMethod
+    public void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        page = browser.newPage();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (page != null) {
+            page.close();
+        }
+        if (browser != null) {
+            browser.close();
+        }
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
+}
